8579eab2ab12a92c6f45166288d5049c4e6d35e3
Gabriel Castro <castrovisionario@icloud.com>
2026-05-17 11:37:41 -0300

    add try catch

--- server.js
+++ server.js
@@ -11,15 +11,20 @@ app.use(cors())
 
 app.get('/usuarios', async (req, res) => {
 
+    try {
     const users =  await prisma.user.findMany()
 
+    return res.status(200).json(users)
+    } catch (err) {
+    return res.status(500).json({ message: 'Erro ao buscar usuários', error: err.message })
+    }
 
-    res.status(200).json(users)
 });
 
 
 app.post('/usuarios', async (req, res) => {
-    
+
+    try {
     const user = await prisma.user.create({
         data: {
             email: req.body.email,
@@ -27,13 +32,16 @@ app.post('/usuarios', async (req, res) => {
             age: req.body.age
         }
     })
-
-    res.status(201).json({ message: 'Usuário criado com sucesso!', user })
+    return res.status(201).json({ message: 'Usuário criado com sucesso!', user })
+    } catch (err) {
+        return res.status(500).json({ message: 'Erro ao criar usuário', error: err.message })
+    }
 });
 
 
 app.put('/usuarios/:id', async (req, res) => {
     
+    try {
     const user = await prisma.user.update({
         where: {
             id: req.params.id
@@ -44,18 +52,24 @@ app.put('/usuarios/:id', async (req, res) => {
             age: req.body.age
         }
     })
-
+    } catch (err) {
+        return res.status(500).json({ message: 'Erro ao atualizar usuário', error: err.message })
+    }
     res.status(200).json({ message: 'Usuário atualizado com sucesso!', user })
 });
 
 
 app.delete('/usuarios/:id', async (req, res) => {
+    try {
     await prisma.user.delete({
         where: {
             id: req.params.id
         }
     })
-    res.status(200).json({ message: 'Usuário deletado com sucesso!' })
+    return res.status(200).json({ message: 'Usuário deletado com sucesso!' })
+    } catch (err) {
+        return res.status(500).json({ message: 'Erro ao deletar usuário', error: err.message })
+    }
 });
 
 app.listen(3000)
